38d362df53ce96c827bc3bee34a9efd4f695a78d
alisha-0045 <77913358+alisha-0045@users.noreply.github.com>
2021-09-11 23:50:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var friends = ["anuj","alisha","lokesh","naman","osman"];
+console.log(friends[4])
+
+var edges = createEdgeSprite();
+
+function setup() {
+  createCanvas(400, 400);
+
+
+
+}
+
+
+
+
+function draw() {
+  background(220);
+
+
+
+
+
+  
+}
